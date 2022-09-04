eb066c2ce4336c6f47c70a9c49b2d6f0f9ee0015
Rizo I <rizo@odis.io>
2022-09-04 19:02:34 +0100

    Implement device+i2c wrapper.

--- sketch_extension.ino
+++ sketch_extension.ino
@@ -1,38 +1,14 @@
-// HYPER: INCLUDES
-#include "hyper_device_17_v3.gen.h"
-#include "hyper_extension_bus.h"
+#include "HyperDevice17I2CSlave.h"
 
-// HYPER: Global device reference.
-hyper_device_17_t hyper_device = {0};
+// Initialize global Hyper device.
+HyperDevice17I2CSlave hyperDevice;
 
-
-// HYPER: Set up I2C set/get helpers.
-static int hyper_extension_set_data(uint8_t *data, uint8_t data_len) {
-  return 0;
-}
-
-static int hyper_extension_get_data(uint8_t *data, uint8_t *data_len) {
-  hyper_result_t ret = hyper_device_17_encode(&hyper_device, data, data_len);
-  if (ret != HYPER_OK) {
-    Serial.println("hyper_device_17_encode failed.");
-    return -1;
-  } else {
-    return 0;
-  }
-}
-
-
-// The setup routine runs once when you press reset.
 void setup() {
-  // Initialize serial communication at 115200 bits per second.
   Serial.begin(115200);
 
-  // HYPER: Initialize the device.
-  uint8_t device_id[6] = {0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x01};
-  hyper_device_17_init(&hyper_device, device_id);
-
-  // HYPER: Setup I2C slave bus.
-  hyper_extension_bus_slave_init(hyper_extension_get_data, hyper_extension_set_data);
+  // Begin the device with a unique identifier.
+  const uint8_t deviceId[6] = {0xFF, 0xFF, 0xFF, 0x00, 0x00, 0x01};
+  hyperDevice.begin(deviceId);
 }
 
 void loop() {
@@ -40,7 +16,9 @@ void loop() {
   Serial.print("Read ADC Value: ");
   Serial.println(adcVal);
 
-  hyper_device_17_set_temperature_1(&hyper_device, adcVal);
+  // Set the attribute value.
+  hyperDevice.setTemperature1(adcVal);
 
   delay(1000);
 }
+
